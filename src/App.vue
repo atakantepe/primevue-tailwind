fbe2294eded2b1d44527ce7a5bbb7480c96b2b4e
Cagatay Civici <cagatay.civici@gmail.com>
2023-09-27 20:01:53 +0300

    Initiate repo

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="main">
+    <div class="card">
+      <a href="https://primevue.org/" target="_blank">
+        <img src="./assets/primevue-icon.svg" class="logo vue" alt="PrimeVue logo" />
+      </a>
+      <h1>Tailwind CSS Based UI Component Library for Vue.js</h1>
+      <p>PrimeVue team is working on a brand new customizable preset implemented with Tailwind CSS.</p>
+      <div class="pbar">
+          <div class="pbar-value"></div>
+      </div>
+      <span>Coming Soon</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
